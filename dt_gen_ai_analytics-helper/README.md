8349d2b2623583bd179a6585effc65f53e636ed5
roberta-dt <roberta.nwokonko@datatonic.com>
2023-09-19 02:09:20 +0100

    fix: fix latency issues with LLM chain

--- dt_gen_ai_analytics-helper/README.md
+++ dt_gen_ai_analytics-helper/README.md
@@ -1,4 +1,5 @@
 ## Datatonic GenAI Hackathon Helper module
+- A gradio app for the Analytics Assistant demo
 - A helper Python module to abstract parts of the Datatonic Gen AI Hackathon workshop. 
 
 ## To run the gradio interface and query the LLM
@@ -9,3 +10,4 @@
 - How many employees are there?
 - What is the total_revenue?
 - How many customers are there?
+- How many tables are there?
--- dt_gen_ai_analytics-helper/main.py
+++ dt_gen_ai_analytics-helper/main.py
@@ -1,22 +1,53 @@
-from dt_gen_ai_analytics_helper.chains.chains import create_db_chain
-import dt_gen_ai_analytics_helper.view.view as demo_view
+from dt_gen_ai_analytics_helper.bigquery.bigquery import biqquery_client
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit 
+from langchain.agents.agent_types import AgentType
+from langchain.sql_database import SQLDatabase
+from langchain.agents import create_sql_agent 
+from sqlalchemy.engine import create_engine
+from langchain.llms import VertexAI
+from google.cloud import aiplatform
 import gradio as gr
+import os
 
 
-# Define a function to query the SQLDBChain
+# instatiate the bigquery and aiplatform client
+client = biqquery_client
+aiplatform.init()
+
+# Define project env variables
+PROJECT_ID = 'dt-gen-ai-hackathon-dev'
+DATASET_ID = 'database_analytics_demo_v2'
+
+# Define database engine and LLM 
+engine = create_engine(f"bigquery://{PROJECT_ID}/{DATASET_ID}", pool_pre_ping=True)
+llm = VertexAI(model_name='text-bison@001',
+               temperature=0, max_output_tokens=1024)
+db = SQLDatabase(engine=engine)
+
+
+# Define our agent’s toolkit which will be used to answer the user question
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+qa_chain = create_sql_agent(
+    llm=llm,
+    db=db, 
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+
+
+# Define a function to query the sql Agent 
 def query_database(question):
-    # Call the SQLDBChain to get the answer based on the question
-    qa_chain = create_db_chain()
-    answer = qa_chain(question)
 
+    answer = qa_chain.run(question)
 
     return answer
+
 
 # Create a Gradio interface
 iface = gr.Interface(
     fn=query_database,  # Function to execute when a query is received
     inputs="text",      # Input is a single text field
-    outputs="json",     # Output will be a JSON response
+    outputs="text",     # Output will be a text response
     title="Analytics Worker Demo",
     description="Enter a question, and the system will query the database and provide an answer.",
 )
