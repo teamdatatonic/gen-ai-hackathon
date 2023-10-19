dd0768daeb09cd8b936f20059acfeefb6f52214d
juan-calvo <juan.calvo@datatonic.com>
2023-10-19 18:10:10 +0200

    Update format_results function to output a dataframe

--- knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
+++ knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
@@ -1,7 +1,9 @@
 from typing import Dict
 
+import pandas as pd
 
-def format_results(results: Dict) -> None:
+
+def format_results(results: Dict) -> pd.DataFrame:
     """
     Formats and prints the results of a question-answering query.
     Args:
@@ -9,19 +11,42 @@ def format_results(results: Dict) -> None:
     Returns:
         None
     """
-    counter = 1
+    # Prepare the header for the table
     print("*" * 79)
     print(f"Answer: {results['result']}")
     print(f"Used {len(results['source_documents'])} relevant documents.")
+
+    # Initialize an empty DataFrame
+    df = pd.DataFrame(columns=['Document', 'Source', 'First 100 characters'])
+
+    # Loop through source documents to populate DataFrame
+    counter = 1
+    for doc in results["source_documents"]:
+        new_row = pd.DataFrame({
+            'Document': f"Dcoument {counter}",
+            'Source': [gsutil_uri_to_gcs_url(doc.metadata['source'])],
+            'First 100 characters': [doc.page_content[:100]]
+        })
+        df = pd.concat([df, new_row], ignore_index=True)
+        counter += 1
+
+    # Print the DataFrame
+    print(df)
+
+    # Print the separator
     print("*" * 79)
+
+    # Continue with the original printing logic
+    counter = 1
     for doc in results["source_documents"]:
         print("-" * 79)
         print(f"Document {counter}")
         print("-" * 79)
-        print(f"Source of content: {doc.metadata['source']}")
+        print(f"Source of content: {gsutil_uri_to_gcs_url(doc.metadata['source'])}")
         print("-" * 79)
         print(doc.page_content)
         counter += 1
+    return df
 
 
 def gsutil_uri_to_gcs_url(gsutil_uri: str) -> str:
@@ -34,4 +59,3 @@ def gsutil_uri_to_gcs_url(gsutil_uri: str) -> str:
         str: The converted GCS URL, e.g., "https://storage.googleapis.com/bucket-name/object-name".
     """
     return gsutil_uri.replace("gs://", "https://storage.googleapis.com/")
-
