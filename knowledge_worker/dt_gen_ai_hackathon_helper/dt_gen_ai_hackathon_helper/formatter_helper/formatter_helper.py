cb86ae0f4b838cb0c603432a00533b282b8bbd3a
juan-calvo <juan.calvo@datatonic.com>
2023-10-19 11:15:31 +0200

    Use package functions and add gsutil_uri_to_gcs_url function

--- knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
+++ knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
@@ -22,3 +22,16 @@ def format_results(results: Dict) -> None:
         print("-" * 79)
         print(doc.page_content)
         counter += 1
+
+
+def gsutil_uri_to_gcs_url(gsutil_uri: str) -> str:
+    """Converts a gsutil URI to a GCS URL.
+
+    Args:
+        gsutil_uri (str): The gsutil URI to be converted, e.g., "gs://bucket-name/object-name".
+
+    Returns:
+        str: The converted GCS URL, e.g., "https://storage.googleapis.com/bucket-name/object-name".
+    """
+    return gsutil_uri.replace("gs://", "https://storage.googleapis.com/")
+
