9e8ec1b40d65685f3f69c92455a5896377ba559d
Felix Schaumann <89205956+felix-datatonic@users.noreply.github.com>
2023-10-23 18:18:10 -0400

    Update formatter_helper.py

--- knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
+++ knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/formatter_helper/formatter_helper.py
@@ -1,55 +1,8 @@
-from typing import Dict
+from langchain.document_loaders.xorbits import XorbitsLoader
+from typing import Any, Dict
 
+from IPython.display import HTML
 import pandas as pd
-
-
-def format_results(results: Dict, print_source=False) -> pd.DataFrame:
-    """
-    Formats and prints the results of a question-answering query.
-    Args:
-        results: The results of a question-answering query.
-    Returns:
-        None
-    """
-    # Display settings for columns
-    pd.set_option('display.max_colwidth', 150)
-    # Prepare the header for the table
-    print("*" * 79)
-    print(f"Answer: {results['result']}")
-    print(f"Used {len(results['source_documents'])} relevant documents.")
-
-    # Initialize an empty DataFrame
-    df = pd.DataFrame(columns=['Document', 'Source', 'First 100 characters'])
-
-    # Loop through source documents to populate DataFrame
-    counter = 1
-    for doc in results["source_documents"]:
-        new_row = pd.DataFrame({
-            'Document': f"Document {counter}",
-            'Source': [gsutil_uri_to_gcs_url(doc.metadata['source'])],
-            'First 100 characters': [doc.page_content[:100]]
-        })
-        df = pd.concat([df, new_row], ignore_index=True)
-        counter += 1
-
-    # Print the DataFrame
-    print(df)
-
-    # Print the separator
-    print("*" * 79)
-
-    if print_source:
-        # Continue with the original printing logic
-        counter = 1
-        for doc in results["source_documents"]:
-            print("-" * 79)
-            print(f"Document {counter}")
-            print("-" * 79)
-            print(f"Source of content: {gsutil_uri_to_gcs_url(doc.metadata['source'])}")
-            print("-" * 79)
-            print(doc.page_content)
-            counter += 1
-    return df
 
 
 def gsutil_uri_to_gcs_url(gsutil_uri: str) -> str:
@@ -62,3 +15,43 @@ def gsutil_uri_to_gcs_url(gsutil_uri: str) -> str:
         str: The converted GCS URL, e.g., "https://storage.googleapis.com/bucket-name/object-name".
     """
     return gsutil_uri.replace("gs://", "https://storage.googleapis.com/")
+
+
+def format_results(results: Dict, print_source=False, return_html=True) -> Any:
+    """
+    Formats and prints the results of a question-answering query.
+    Args:
+        results: The results of a question-answering query.
+    Returns:
+        None
+    """
+    sep = "*" * 79
+    docs = results['source_documents']
+    # Display settings for columns
+    pd.set_option('display.max_colwidth', 80)
+    # Prepare the header for the table
+    print(sep)
+    print(f"Answer: {results['result']}")
+    print(f"Used {len(docs)} relevant documents.")
+    print(sep)
+
+    # Populate DataFrame
+    records = [
+        (gsutil_uri_to_gcs_url(doc.metadata['source']), 
+         doc.page_content[:100] + "...") 
+        for doc in docs
+    ]
+    df = pd.DataFrame.from_records(records, columns=["Source", "Preview"])
+
+    if print_source:
+        for idx, doc in enumerate(docs):
+            print(sep)
+            print(f"Document: {idx}")
+            print(f"Source: {gsutil_uri_to_gcs_url(doc.metadata['source'])}")
+            print("Content:")
+            print(doc.page_content)
+    
+    if return_html:
+        return HTML(df.to_html(render_links=True, escape=False))
+
+    return df
