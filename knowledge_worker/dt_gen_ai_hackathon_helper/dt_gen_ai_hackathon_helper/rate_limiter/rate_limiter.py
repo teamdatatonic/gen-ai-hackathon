697bde80d7160ba62712382e53ba0cb0f4d718f3
Romain <rom1.allwhite@gmail.com>
2023-10-25 12:38:52 -0400

    add rate limiter + customvertexai embeddings

---
+++ knowledge_worker/dt_gen_ai_hackathon_helper/dt_gen_ai_hackathon_helper/rate_limiter/rate_limiter.py
@@ -0,0 +1,16 @@
+import time
+
+
+# Utility functions for Embeddings API with rate limiting
+def rate_limit(max_per_minute):
+    period = 60 / max_per_minute
+    print("Waiting")
+    while True:
+        before = time.time()
+        yield
+        after = time.time()
+        elapsed = after - before
+        sleep_time = max(0, period - elapsed)
+        if sleep_time > 0:
+            print(".", end="")
+            time.sleep(sleep_time)
