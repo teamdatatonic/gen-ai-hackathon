216be81a79dbe38a00510d5c10855891e0a5257a
Zachary <zachary.smith@datatonic.com>
2023-12-04 11:52:45 +0000

    Updated instructions for langsmith testing

--- langchain-tech-stack-hackathon/.envrc.example.sh
+++ langchain-tech-stack-hackathon/.envrc.example.sh
@@ -1,7 +1,7 @@
 export LANGCHAIN_TRACING_V2=true
 export LANGCHAIN_ENDPOINT=https://api.smith.langchain.com
 export LANGCHAIN_API_KEY=<your-langsmith-api-key>
-export LANGCHAIN_PROJECT=rag-google-cloud-vertexai-search  # if not specified, defaults to "default"
+export LANGCHAIN_PROJECT=hackathon-logging  # if not specified, defaults to "default"
 
 export GOOGLE_CLOUD_PROJECT_ID=dt-gen-ai-hackathon-dev
 export DATA_STORE_ID=<your-data-store-id>
